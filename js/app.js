2c0ce92eb1f1c1b06d905a41a8c442f14b36fd02
Stefano.In91 <ste.ince91@gmail.com>
2022-11-11 17:06:05 +0100

    adds more images for the fun

--- js/app.js
+++ js/app.js
@@ -6,8 +6,24 @@ createApp({
   data() {
     return {
       message: "Hello Vue!",
-      myImage:
-        "https://i1.sndcdn.com/artworks-000066021724-kq5xql-t500x500.jpg",
+      myImages: [
+        {
+          image:
+            "https://i1.sndcdn.com/artworks-000066021724-kq5xql-t500x500.jpg",
+        },
+        {
+          image:
+            "https://i.kym-cdn.com/photos/images/newsfeed/000/296/441/24a.jpg",
+        },
+        {
+          image:
+            "https://media.makeameme.org/created/whats-up-motherfucker.jpg",
+        },
+        {
+          image:
+            "https://media.tenor.com/images/f07a41a9e6009bbb325286a669ae519b/tenor.png",
+        },
+      ],
     };
   },
 }).mount("#app");
