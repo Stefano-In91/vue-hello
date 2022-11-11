da2950d22e0d54ec143d6ca9a012beea39c5fe03
Stefano.In91 <ste.ince91@gmail.com>
2022-11-11 16:49:14 +0100

    adds vue image

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ createApp({
   data() {
     return {
       message: "Hello Vue!",
+      myImage:
+        "https://i1.sndcdn.com/artworks-000066021724-kq5xql-t500x500.jpg",
     };
   },
 }).mount("#app");
